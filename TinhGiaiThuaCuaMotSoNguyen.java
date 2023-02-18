26917b7f00409a61f65ef342f950ecea9833de5b
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-02-18 23:05:22 +0700

    Create TinhGiaiThuaCuaMotSoNguyen.java

---
+++ TinhGiaiThuaCuaMotSoNguyen.java
@@ -0,0 +1,21 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class TinhGiaiThuaCuaMotSoNguyen {
+    public static void main(String[] args) {
+        // Nhập số nguyên dương n
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao mot so nguyen duong: ");
+        int n = scanner.nextInt();
+
+        // Tính giai thừa của n
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+
+        // In kết quả
+        System.out.println(n + "! = " + factorial);
+    }
+}
